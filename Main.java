f17efaf8c391888fc5a1ff828efbdbc548d1cd63
syedmgraza <syedmgraza@gmail.com>
2026-04-22 17:31:52 +0530

    adding readme and changes from Main

--- Main.java
+++ Main.java
@@ -1,9 +1,13 @@
 package FirstProject;
 
 public class Main {
+    static class DB{
+        DB(){}
+    }
     public static void main(String args[]){
         System.out.println("Hello this is Main");
         System.out.println("Added Another Line In Main");
         System.out.println("added a line");
+        DB db=new DB();
     }
 }
