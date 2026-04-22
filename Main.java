37b2e3f3264e412da9d430d27b9ab58c4eb8f0b1
syedmgraza <syedmgraza@gmail.com>
2026-04-22 16:47:29 +0530

    Added a Message in Main

--- Main.java
+++ Main.java
@@ -3,5 +3,6 @@ package FirstProject;
 public class Main {
     public static void main(String args[]){
         System.out.println("Hello this is Main");
+        System.out.println("Added Another Line In Main");
     }
 }
