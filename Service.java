81314610c4bc8457f396d8d7d5ff41b8346c26d9
syedmgraza <syedmgraza@gmail.com>
2026-04-26 23:19:22 +0530

    experimenting with service in FirstProject

---
+++ Service.java
@@ -0,0 +1,13 @@
+package FirstProject;
+
+//adding this new service to the code base of the first project but not in main branch in another branch
+public class Service {
+
+    public void getName(){
+        System.out.println("syed raza");
+    }
+    public static void main(String args[]){
+        Service sr=new Service();
+        sr.getName();
+    }
+}
